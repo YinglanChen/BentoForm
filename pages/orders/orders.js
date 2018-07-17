6c7b4394eeba2869231434050302933b9408b9ac
MirandaChen <yinglanc@andrew.cmu.edu>
2018-07-16 23:24:13 -0400

    implement orders page

--- pages/orders/orders.js
+++ pages/orders/orders.js
@@ -2,17 +2,12 @@ Page({
   data: {
     listData: [
       { "code": "01", "text": "text1", "type": "type1" },
-      { "code": "02", "text": "text2", "type": "type2" },
-      { "code": "03", "text": "text3", "type": "type3" },
-      { "code": "04", "text": "text4", "type": "type4" },
-      { "code": "05", "text": "text5", "type": "type5" },
-      { "code": "06", "text": "text6", "type": "type6" },
-      { "code": "07", "text": "text7", "type": "type7" }
+      { "code": "02", "text": "text2", "type": "type2" }
     ],
-    items: [
-      { name: 'USA', value: '美国' },
-      { name: 'CHN', value: '中国', checked: 'true' },
-      { name: 'BRA', value: '巴西' },
+    filters: [
+      { name: 'byOrder', value: '按订单查看' },
+      { name: 'byPlace', value: '按地点查看', checked: 'true' },
+      { name: 'byMenu', value: '按菜品查看' },
 
     ]
   },
