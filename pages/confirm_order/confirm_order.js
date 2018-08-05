fd481a190eb516a489688b5b9d2d7794227f54ac
ruoyuanl <ruoyuanl@andrew.cmu.edu>
2018-08-05 23:59:13 +0800

    basic data and functions setup

--- pages/confirm_order/confirm_order.js
+++ pages/confirm_order/confirm_order.js
@@ -4,14 +4,33 @@ Page({
    * 页面的初始数据
    */
   data: {
-    
+    name: "确认订单-",
+    confirmed: [],
+    locations: ["Hunt", "Gates"]
+  },
+
+  getBack: function (e) {
+    wx.navigateBack({
+      delta: 1
+    })
+  },
+
+  /* upload "confirmed", "time" and "place" to database */
+  confirm: function (e) {
+
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    
+    console.log(options.confirm)
+    var res = options.res
+    var menu = JSON.parse(options.confirm)
+    this.setData({
+      name: this.data.name + res,
+      confirmed: menu
+    })
   },
 
   /**
