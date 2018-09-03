36b50b741a2a9c4933157c5d14780b80d55230ce
ruoyuanl <ruoyuanl@andrew.cmu.edu>
2018-09-03 16:20:22 -0400

    some features added (not debugged)

--- pages/res_orders/res_orders.js
+++ pages/res_orders/res_orders.js
@@ -1,23 +1,56 @@
 Page({
   data: {
-    listData: [
-      { "code": "01", "text": "text1", "type": "type1" },
-      { "code": "02", "text": "text2", "type": "type2" }
-    ],
+    res_id: -1,
+    /*listByOrder: [
+      { "cus_id": "Sudoku", "price": "$1", "location": "Gates" },
+      { "cus_id": "Sudoku2", "price": "$100", "location": "Hunt" }
+    ],*/
+    listByOrder: [],
     filters: [
-      { name: 'byOrder', value: '按订单查看' },
-      { name: 'byPlace', value: '按地点查看', checked: 'true' },
-      { name: 'byMenu', value: '按菜品查看' },
+      { name: 'byOrder', value: '按订单查看', checked: false },
+      { name: 'byPlace', value: '按地点查看', checked: false },
+      { name: 'byMenu', value: '按菜品查看', checked: false },
 
     ]
   },
   onLoad: function () {
-    console.log('onLoad')
+    this.setData({
+      res_id: options.id
+    })
   },
-
 
   radioChange: function (e) {
     console.log('radio发生change事件，携带value值为：', e.detail.value)
+    var that = this
+    var fil = this.data.filters
+    for (var i = 0; i < fil.length; i++) {
+      if (fil[i].name == e.detail.value) {
+        fil[i].checked = true
+        wx.request({
+          url: 'https://www.alphalunch.xyz/res/currentorder',
+          data: {
+            rid: res_id,
+            key: 's',
+            value: 0x7eac8fde1aa076c4e16502cf85980562
+          },
+          method: 'GET',
+          header: {
+            'Content-Type': 'application/json'
+          },
+          success: function (res) {
+            that.setData({
+              filters: res.data
+            })
+          }
+        })
+      }
+      else {
+        fil[i].checked = false
+      }
+    }
+    this.setData({
+      filters: fil
+    })
   }
 
 })
