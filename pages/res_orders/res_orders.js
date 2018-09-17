acf9a675b402a0fb2375b9c82332a7898624a4e3
ruoyuanl <ruoyuanl@andrew.cmu.edu>
2018-09-17 14:49:01 -0400

    database connected

--- pages/res_orders/res_orders.js
+++ pages/res_orders/res_orders.js
@@ -7,42 +7,43 @@ Page({
     ],*/
     listByOrder: [],
     filters: [
-      { name: 'byOrder', value: '按订单查看', checked: false },
+      { name: 'byOrder', value: '按订单查看', checked: true },
       { name: 'byPlace', value: '按地点查看', checked: false },
       { name: 'byMenu', value: '按菜品查看', checked: false },
 
     ]
   },
-  onLoad: function () {
+
+  onLoad: function (options) {
     this.setData({
       res_id: options.id
+    })
+    var that = this
+    wx.request({
+      url: 'https://www.alphalunch.xyz/bento/res/currentorder',
+      data: {
+        rid: this.data.res_id,
+        s: "7eac8fde1aa076c4e16502cf85980562"
+      },
+      method: 'GET',
+      header: {
+        'Content-Type': 'application/json'
+      },
+      success: function (res) {
+        console.log(res.data)
+        that.setData({
+          listByOrder: res.data
+        })
+      }
     })
   },
 
   radioChange: function (e) {
     console.log('radio发生change事件，携带value值为：', e.detail.value)
-    var that = this
     var fil = this.data.filters
     for (var i = 0; i < fil.length; i++) {
       if (fil[i].name == e.detail.value) {
         fil[i].checked = true
-        wx.request({
-          url: 'https://www.alphalunch.xyz/res/currentorder',
-          data: {
-            rid: res_id,
-            key: 's',
-            value: 0x7eac8fde1aa076c4e16502cf85980562
-          },
-          method: 'GET',
-          header: {
-            'Content-Type': 'application/json'
-          },
-          success: function (res) {
-            that.setData({
-              filters: res.data
-            })
-          }
-        })
       }
       else {
         fil[i].checked = false
